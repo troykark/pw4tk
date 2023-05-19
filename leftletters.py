160c176a97f9a53b3dd5d953f37fbbf341c538f8
troykark <troykark@gmail.com>
2023-05-19 07:37:37 -0500

    first

---
+++ leftletters.py
@@ -0,0 +1,8 @@
+leftletters = [['q','a','z'],['w','s','x'],['e','d','c'],['r','f','v'],['t','g','b']]
+dictionary = open("C:\\users\\tkarkula\\desktop\\pw4tk\\wordlist.txt","r")
+words = dictionary.readlines()
+
+
+print(leftletters[0][1])
+print(words)
+print(words[3])
